792cf3029d6d5450601a92b4fa6a27a89a3ffcb5
Illia Horevoi <illia.horevoi@gmail.com>
2023-05-20 01:46:30 +0300

    Initial Commit

---
+++ TestingApolloAPI/TestingApolloAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApolloAPIApp.swift
+//  TestingApolloAPI
+//
+//  Created by ILLIA HOREVOI on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestingApolloAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
