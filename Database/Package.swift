aa6f0f6e913114ef56bf39ee324b083d8a07154f
Illia Horevoi <illia.horevoi@gmail.com>
2023-05-20 05:34:04 +0300

    Package fix

--- Database/Package.swift
+++ Database/Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,22 +8,24 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Database",
             targets: ["Database"]),
     ],
     dependencies: [
-        .package(name: "NetworkLayer", path: "../NetworkLayer")
+        .package(name: "NetworkLayer", path: "../NetworkLayer"),
+        .package(url: "https://github.com/apollographql/apollo-ios.git", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Database",
-            dependencies: ["NetworkLayer"]),
+            dependencies: ["NetworkLayer",
+              .product(name: "Apollo", package: "apollo-ios")
+            ]),
         .testTarget(
-            name: "DatabaseTests",
-            dependencies: ["Database", "NetworkLayer"]),
+          name: "Database",
+          dependencies: ["Database", "NetworkLayer",
+                         .product(name: "Apollo", package: "apollo-ios")
+          ])
     ]
 )
