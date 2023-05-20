121b33e5c884ddb61d1ce96e0450df3d439b78e0
Illia Horevoi <illia.horevoi@gmail.com>
2023-05-20 05:21:05 +0300

    Small fix

--- Database/Sources/Database/Database.swift
+++ Database/Sources/Database/Database.swift
@@ -1,12 +1,5 @@
 import Foundation
-import NetworkLayer
 
 public struct Database {
-  
-  public init() {
-    apolloClient.fetch(query: CountOfFilmsQuery()) { result in
-      let data = try? result.get()
-
-    }
-  }
+  init() { }
 }
--- TestingApolloAPI/TestingView/TestingVIewViewModel.swift
+++ TestingApolloAPI/TestingView/TestingVIewViewModel.swift
@@ -1,5 +1,4 @@
 import Foundation
-import Database
 
 class TestingViewViewModel: ObservableObject {
   init() {
